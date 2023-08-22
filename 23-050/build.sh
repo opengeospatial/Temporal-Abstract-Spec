6843ee0265c619e5e8527be32cb3a8ae1ae2b28b
Chuck Heazel <cmheazel@users.noreply.github.com>
2023-08-22 19:51:34 -0400

    Draft for review

---
+++ 23-050/build.sh
@@ -0,0 +1,1 @@
+docker run -v /mnt/c/WorkSpace/GitHub/Temporal-Abstract-Spec/23-049:/metanorma metanorma/mn metanorma compile --agree-to-terms -t ogc -x xml,doc,pdf,html 23-049.adoc
---
+++ 23-050/sections/images/readme.md
@@ -0,0 +1,3 @@
+This folder contains some of the images used in the temporal documents.
+
+Others are expressed directly as Mermaid or PlantUML diagrams.
